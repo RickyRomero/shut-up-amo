da584bcba371403ca1c84d068792a0ce243ad059
Ricky Romero <ricky.romero@gmail.com>
2022-09-14 20:35:33 -0700

    Remove prebuild step and copy Firefox-specific manifest

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
+
+echo "Setting options to prevent cascading errors..."
 set -eu
 
+echo "Copying extension files..."
 cd /usr/app
 cp -R $GITHUB_WORKSPACE/* /amo/extension/
 
-yarn prebuild
+echo "Selecting Firefox manifest..."
+rm /amo/extension/manifest.json
+mv /amo/extension/manifest.ffx.json /amo/extension/manifest.json
+
+echo "Building extension..."
 yarn build
+
+echo "Submitting extension..."
 yarn submit
